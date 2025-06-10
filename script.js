0af30246f7edd39af2fcc66faa572b3789902ded
kenzelsherbini <526kelsherbini@frhsd.com>
2025-06-10 09:18:31 -0400

    styled carasoul

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('send-button').addEventListener('click', () => {
+            // Hide feedback form and show thank-you message
+            document.getElementById('feedback-card').classList.add('d-none');
+            document.getElementById('thank-you-card').classList.remove('d-none');
+        });
