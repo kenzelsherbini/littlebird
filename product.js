5d9d8f63bdc28b4c5986bfba27082d6502ed02f3
kenzelsherbini <526kelsherbini@frhsd.com>
2025-06-11 10:29:29 -0400

    added display img hovers

--- product.js
+++ product.js
@@ -6,4 +6,4 @@ const product = [
     age: 1981,
     photo: "imgs/m1.jpg",
     hiddenDetail: "The M1 was BMW's first M car, created in collaboration with Lamborghini. It was a mid-engine supercar with only 453 units made."
-  },s
+  },
