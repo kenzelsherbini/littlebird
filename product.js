f0583cf4e452c23bc823d9a1434f793690cdc4ab
kenzelsherbini <526kelsherbini@frhsd.com>
2025-06-23 09:10:21 -0400

    fixed colors and added a navbar to the contacts

--- product.js
+++ product.js
@@ -1,4 +1,3 @@
-
 const modalContainer = document.createElement("div");
 
 modalContainer.innerHTML = `
@@ -19,13 +18,10 @@ modalContainer.innerHTML = `
   </div>
 `;
 
-
 document.body.appendChild(modalContainer);
 
 const firstRowPlayers = [
-
   {
-
     firstName: "Wooden",
 
     lastName: "Boat",
@@ -33,25 +29,19 @@ const firstRowPlayers = [
     price: 25,
 
     photos: ["imgs/boat1.jpg", "imgs/boat2.jpg", "imgs/boat1.jpg"],
-
   },
 
   {
-
     firstName: "Wooden",
-
-    
 
     lastName: "Plane",
 
     price: 60,
 
     photos: ["imgs/plane1.jpg", "imgs/plane2.jpg", "imgs/plane3.jpg"],
-
   },
 
   {
-
     firstName: "Wooden",
 
     lastName: "Train",
@@ -59,15 +49,12 @@ const firstRowPlayers = [
     price: 25,
 
     photos: ["imgs/train2.jpg", "imgs/train1.jpg", "imgs/train4.jpg"],
-  }
+  },
 ];
-
-
 
 // Second row of products
 
 const secondRowPlayers = [
-
   {
     firstName: "Wooden",
 
@@ -75,32 +62,25 @@ const secondRowPlayers = [
 
     price: 40,
 
-    photos: ["imgs/block1.jpg", "imgs/block2.jpg",],
-
+    photos: ["imgs/block1.jpg", "imgs/block2.jpg"],
   },
 
   {
-
-     firstName: "Wooden",
+    firstName: "Wooden",
 
     lastName: "Car",
 
     price: 45,
 
     photos: ["imgs/car1.jpg", "imgs/car2.jpg", "imgs/car3.jpg"],
-    
-
-  }
-
+  },
 ];
 
 function renderPlayers(list, rowElement, indexOffset) {
-
   for (let i = 0; i < list.length; i++) {
-
     const p = list[i];
 
-    const globalIndex = indexOffset + i; 
+    const globalIndex = indexOffset + i;
     const card = document.createElement("div");
 
     card.className = "card";
@@ -142,28 +122,20 @@ function renderPlayers(list, rowElement, indexOffset) {
 
     `;
 
-
     rowElement.appendChild(card);
-
 
     const qtySelect = document.getElementById(`qty-${globalIndex}`);
 
     const priceSpan = document.getElementById(`price-${globalIndex}`);
 
     qtySelect.onchange = function () {
-
       priceSpan.textContent = p.price * qtySelect.value;
-
     };
-
 
     const moreInfoButton = document.getElementById(`more-info-${globalIndex}`);
 
     moreInfoButton.onclick = function () {
-
       const modalBody = document.getElementById("modalBody");
-
-
 
       modalBody.innerHTML = `
 
@@ -178,32 +150,28 @@ function renderPlayers(list, rowElement, indexOffset) {
           ${p.photos
 
             .map(
-
               (photo, index) =>
-
-                `<img src="${photo}" alt="Photo ${index + 1}" style="width: 50px; margin: 5px;">`
-
+                `<img src="${photo}" alt="Photo ${
+                  index + 1
+                }" style="width: 50px; margin: 5px;">`
             )
 
             .join("")}
 
         </div>
 
-        <p>This is a beautiful handcrafted wooden ${p.lastName}. Perfect for kids and collectors alike!</p>
+        <p>This is a beautiful handcrafted wooden ${
+          p.lastName
+        }. Perfect for kids and collectors alike!</p>
 
       `;
-
     };
-
   }
-
 }
 
-
-document.addEventListener('DOMContentLoaded', function() {
-  const row1 = document.getElementById("row1"); 
+document.addEventListener("DOMContentLoaded", function () {
+  const row1 = document.getElementById("row1");
   const row2 = document.getElementById("row2");
   renderPlayers(firstRowPlayers, row1, 0);
   renderPlayers(secondRowPlayers, row2, firstRowPlayers.length);
 });
-
